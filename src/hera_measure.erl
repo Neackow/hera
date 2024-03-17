6c02da5b33f9ce4ab75727401341751351c05e7f
Neackow <nicolas.isenguerre@student.uclouvain.be>
2024-03-17 23:03:04 +0100

    Oops forgot something

--- src/hera_measure.erl
+++ src/hera_measure.erl
@@ -203,7 +203,6 @@ measure(State=#state{name=N, mod=M, mod_state=MS, seq=Seq, iter=Iter}) ->
                 nav3 ->
                     hera_com:send(N, Seq, Vals);
                 e11 ->
-                    {NowMegaS, NowS} = erlang:timestamp(),
                     output_log_spec("Hera_measure:measure. Iter is = ~p, calling hera_com:send~n",[Iter]),
                     hera_com:send(N, Seq, Vals), % This will call hera_com:send(N, Seq, Vals), from the loop function, when the message is authorized.
                     output_log_spec("Hera_measure:measure after hera_com:send !~n",[])
