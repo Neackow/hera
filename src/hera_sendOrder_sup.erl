d889ebf0cc8427fba66e4c9264dc4ee798bcf619
Neackow <nicolas.isenguerre@student.uclouvain.be>
2024-04-14 13:59:52 +0200

    Cleaning of the code, implementation of a supervisor for tests

---
+++ src/hera_sendOrder_sup.erl
@@ -0,0 +1,20 @@
+-module(hera_sendOrder_sup).
+-behaviour(supervisor).
+ 
+-export([start_link/0]).
+-export([init/1]).
+
+
+start_link() ->
+  supervisor:start_link({local,?MODULE}, ?MODULE, []).
+
+init(_Args) ->
+    SupFlags = #{strategy => one_for_one, intensity => 1, period => 5},
+    ChildSpecs = [#{id => test_hera_sendOrder,
+                    start => {hera_sendOrder, start_link, []},
+                    restart => transient,
+                    shutdown => 5000,
+                    type => worker,
+                    modules => [hera_sendOrder]}],  
+    {ok, {SupFlags, ChildSpecs}}.
+
